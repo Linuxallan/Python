d760e5b3f8a2715b56d3cdf2b5fb0837da6b6ef8
Linuxallan <allancortezvd@gmail.com>
2021-05-26 19:58:36 -0400

    Este archivo corresponde a la clase hasta la 4 del curso de Python de pildoritas informaticas.

---
+++ Class1.py
@@ -0,0 +1,41 @@
+print("hola mundo")
+print("Hi"); print("dots")
+# Comentario
+var_1 = "Hi, manson"
+var_1
+print(var_1)
+print("Este texo es muy larrrrr\
+geeee")
+
+# Mod ->    %
+num = 10%3  # es: 1
+print(num)
+
+# sacar el exponente ->     **
+numEx = 5**3
+print(numEx)
+
+# type() -->    develve el tipo del objeto
+nombre = 4
+print(type(nombre))
+# Python es un lenguaje orientado a objeto
+# las variables son en realidad objetos, instancias de una clase.
+
+# mesajes con salto de lineas ->    """
+mensaje = """Este es un mensaje
+con tres
+o cuatro
+o cinco
+saltos de linea"""
+print(mensaje)
+
+# condicional if
+num1 = 5
+num2 = 5
+if num1 > num2:
+    print("numero 1 es mayor")
+elif num1 == num2:
+    print("Son iguales")
+else:
+    print("num 2 is biggest")
+
