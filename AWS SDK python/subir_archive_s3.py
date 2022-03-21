e118bbb519afc717148cf5aaab31f24a89fe9ff5
Linuxallan <allancortezvd@gmail.com>
2022-03-21 15:43:46 -0300

    solo cambios

--- AWS SDK python/subir_archive_s3.py
+++ AWS SDK python/subir_archive_s3.py
@@ -3,8 +3,10 @@ from botocore.client import Config
 
 BUCKET_NAME = 'qse503208849944'
 
+img = "./img_video/catalina.png"
+
 s3 = boto3.resource('s3')
-data = open('img_video/catalina.png', 'rb')
+data = open(img, 'rb')
 
 s3.Bucket(BUCKET_NAME).put_object(key='catalina.png', body=data)
 
