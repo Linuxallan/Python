ba633adf17414e28abf8ec1c5932209646a606e4
Linuxallan <allancortezvd@gmail.com>
2022-03-22 16:44:40 -0300

    otros datos cw

--- AWS SDK python/doc_2_CW.py
+++ AWS SDK python/doc_2_CW.py
@@ -7,7 +7,7 @@ import boto3
 cw = boto3.client('cloudwatch')
 
 cw.put_metric_alarm(
-    AlarmName='Web_Server_CPU_Utilizationn',
+    AlarmName='Web_Server_CPU_Utilization',
     ComparisonOperator='GreaterThanThreshold',
     EvaluationPeriods=1,
     MetricName='CPUUtilization',
@@ -16,6 +16,9 @@ cw.put_metric_alarm(
     Statistic='Average',
     Threshold=70.0,
     ActionsEnabled=False,
+    AlarmActions=[
+      'arn:aws:swf:us-west-2:503208849944:action/actions/AWS_EC2.InstanceId.Reboot/1.0'
+    ],
     AlarmDescription='Alarm when server CPU exceeds 70%',
     Dimensions=[
         {
@@ -24,6 +27,10 @@ cw.put_metric_alarm(
         },
     ],
     Unit='Seconds'
+)
+# deshabilitar action de la alarma: 'Web_Server_CPU_Utilization'
+cw.disable_alarm_actions(
+  AlarmNames=['Web_Server_CPU_Utilization'],
 )
 
 #-----------------
@@ -36,3 +43,42 @@ paginator = cw.get_paginator('describe_alarms')
 for response in paginator.paginate(StateValue='INSUFFICIENT_DATA'):
     res = response['MetricAlarms']
     print("Metrica: {} \n".format(res))
+
+
+# Eliminar especifica alarma
+cw.delete_alarms(
+    AlarmNames = ['Web_Server_CPU_Utilizationn'],
+)
+
+# Listar metricas de la alarma
+paginator = cw.get_paginator('list_metrics')
+for response in paginator.paginate(Dimensions=[{'Name':'LogGroupName'}],
+                                    MetricName='IncomingLogEvents',
+                                    Namespace='AWS/Logs'):
+    print(response['Metrics'])
+
+
+# Poner metricas, se ve en 'Metrics' en la consola grafica de AWS
+# con el nombre del 'Namespace' : SITE/TRAFFIC.
+cw = boto3.client('cloudwatch')
+
+cw.put_metric_data(
+    MetricData=[
+        {
+            'MetricName': 'PAGES_VISITED',
+            'Dimensions': [
+                {
+                    'Name': 'UNIQUE_PAGES',
+                    'Value': 'URLS'
+                },
+            ],
+            'Unit': 'None',
+            'Value': 1.0
+        },
+    ],
+    Namespace = 'SITE/TRAFFIC'
+)
+
+cw.delete_alarms(
+    AlarmNames = ['Web_Server_CPU_Utilization'],
+)
